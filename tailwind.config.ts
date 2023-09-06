4158b9669371256346b248e391702a7ddff63258
Josh Sanger <joshsanger88@gmail.com>
2023-09-05 21:02:24 -0400

    Tailwind config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,9 +1,39 @@
-import type { Config } from "tailwindcss";
+import type {Config} from 'tailwindcss';
+
+import {
+  animation,
+  backgroundImages,
+  borderRadius,
+  boxShadow,
+  colors,
+  fontFamily,
+  fontSize,
+  keyframes,
+  maxWidth,
+  minWidth,
+  minHeight,
+  screens,
+  spacing,
+} from '~/styles/tailwindOverrides';
 
 export default {
-  content: ["./app/**/*.{js,jsx,ts,tsx}"],
+  content: ['./app/**/*.{js,jsx,ts,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      animation,
+      backgroundImages,
+      borderRadius,
+      boxShadow,
+      colors,
+      fontFamily,
+      fontSize,
+      keyframes,
+      maxWidth,
+      minWidth,
+      minHeight,
+      screens,
+      spacing,
+    },
   },
   plugins: [],
 } satisfies Config;
